d570b0289cdb7fd67c6d8fc9c74b446c4565de61
Juan Carlos <juancarlosenrique103@gmail.com>
2020-04-23 06:41:34 -0500

    Terminando interfaz :)

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,18 @@
+document.querySelectorAll('.btn-open-modal').forEach(btn =>{
+    btn.addEventListener('click', function(){
+        document.getElementById('overlay').classList.add('is-visible');
+        document.getElementById('modal').classList.add('is-visible');
+    });
+})
+
+document.getElementById('close-btn').addEventListener('click', function(){
+    document.getElementById('overlay').classList.remove('is-visible');
+    document.getElementById('modal').classList.remove('is-visible');
+
+});
+
+document.getElementById('overlay').addEventListener('click', function() {
+    document.getElementById('overlay').classList.remove('is-visible');
+    document.getElementById('modal').classList.remove('is-visible');
+});
+  
